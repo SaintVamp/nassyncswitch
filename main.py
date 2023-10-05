b99fab6b1d19a7972147e83c7f486b234fe60a8d
admin <saint-wp@163.com>
2023-10-06 01:09:22 +0800

    update

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ local_conn = sqlite3.connect("count.sqlite")
 local_cur = local_conn.cursor()
 local_cur.execute(
     'CREATE TABLE IF NOT EXISTS status(id INTEGER PRIMARY KEY,local_mtime INTEGER,local_file_size INTEGER,timestamp INTEGER,number INTEGER)')
-conn = sqlite3.connect('/volume1/@cloudsync/session/2/event-db.sqlite')
+# conn = sqlite3.connect('/volume1/@cloudsync/session/2/event-db.sqlite')
+conn = sqlite3.connect('event-db.sqlite')
 cur = conn.cursor()
 cur.execute('select max(id) from event_info')
 v_id = cur.fetchone()[0]
-os.system("echo '-1:" + str(time.time()) + " v_id is " + v_id + " '>> /volume1/run.log")
+os.system("echo '-1:" + str(time.time()) + " v_id is " + str(v_id) + " '>> /volume1/run.log")
 cur.execute(
     'select id,local_mtime,mtime,local_file_size,file_size,timestamp,file_type from event_info where id =' + str(v_id))
 v_data = cur.fetchone()
@@ -27,6 +28,7 @@ if local_data == None:
     local_conn.commit()
     os.system("echo '0:" + str(time.time()) + " init data '>> /volume1/run.log")
 else:
+    os.system("echo '01:" + str(local_data[0]) + " " + str(v_data[0]) + " init data '>> /volume1/run.log")
     if local_data[0] == v_data[0]:  # id是否相同
         if v_data[6] == 1:  # 同步的是否是目录
             if v_data[1] + v_data[5] == local_data[1] + local_data[3]:  # 判断local_mtime+timestamp是否一致

e17fbe2efd06241b11535e91cd49dee2eaf2d915
admin <saint-wp@163.com>
2024-01-12 16:16:34 +0800

    update

--- init-nas_sync.sh
+++ init-nas_sync.sh
@@ -1,4 +1,4 @@
-hostname=`echo hostname`
+hostname=`hostname`
 if [ "$hostname" = "SV-NAS2" ]; then
     curl -o /volume1/start.sh https://gitee.com/saintvamp/nassyncswitch/raw/master/start.sh
     sed -i 's/name=/name=NAS2/g' /volume1/start.sh

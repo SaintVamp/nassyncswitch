81e1871230efe8a532499a28231df5773af3d89f
admin <saint-wp@163.com>
2024-01-12 21:56:20 +0800

    update

--- init-nas-sync.sh
+++ init-nas-sync.sh
@@ -5,5 +5,11 @@ if [ "$hostname" = "SV-NAS2" ]; then
     curl -o /volume1/sync/stop.sh https://gitee.com/saintvamp/nassyncswitch/raw/master/stop.sh
     sed -i 's/hostname=an/hostname=NAS2/g' /volume1/sync/stop.sh
     sh /volume1/sync/start.sh
+elif [ "$hostname" = "SV-NAS3" ]; then
+    curl -o /volume1/sync/start.sh https://gitee.com/saintvamp/nassyncswitch/raw/master/start.sh
+    sed -i 's/name=/name=NAS3/g' /volume1/sync/start.sh
+    curl -o /volume1/sync/stop.sh https://gitee.com/saintvamp/nassyncswitch/raw/master/stop.sh
+    sed -i 's/hostname=an/hostname=NAS3/g' /volume1/sync/stop.sh
+    sh /volume1/sync/start.sh
 fi
 
--- stop.sh
+++ stop.sh
@@ -6,5 +6,11 @@ if [ "$hostname" = "NAS2" ]; then
     sed -i 's/\/session\/2/\/session\/1/g' /volume1/sync/checkdata.py
     sed -i 's/name=/name=NAS2/g' /volume1/sync/exec.sh
     python3 /volume1/sync/checkdata.py
+elif [ "$hostname" = "NAS3" ]; then
+    curl -o /volume1/sync/checkdata.py https://gitee.com/saintvamp/nassyncswitch/raw/master/checkdata.py
+    curl -o /volume1/sync/speed.sh  https://gitee.com/saintvamp/nassyncswitch/raw/master/speed.sh
+    curl -o /volume1/sync/exec.sh https://gitee.com/saintvamp/nassyncswitch/raw/master/exec.sh
+    sed -i 's/name=/name=NAS3/g' /volume1/sync/exec.sh
+    python3 /volume1/sync/checkdata.py
 fi
 
